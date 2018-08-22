674eaece33ab618b295e30c57144302b1f7ed01c
Martin Vondráček <vondracek.mar@gmail.com>
2018-08-22 18:08:55 -0400

    testing server: do not respond

--- server.py
+++ server.py
@@ -6,20 +6,16 @@ import asyncio
 import websockets
 import msgpack
 
+
 async def server(websocket, path):
     print("connected")
     while True:
         msg_raw = await websocket.recv()
-        print(f"< recv:{msg_raw}")
-        #msg = msgpack.unpackb(msg_raw)
-        msg = msg_raw
-        print(f"# unpacked:{msg}")
+        msg = msgpack.unpackb(msg_raw)
+        print(f"< msg:{msg}, msg_raw:{msg_raw}")
 
-        #msg_raw = msgpack.packb(b'replty to ' + msg)
-        #await websocket.send(msg_raw)
-        #print(f"> send:{msg_raw}")
 
-start_server = websockets.serve(server, '127.0.0.1', 8081)
+start_server = websockets.serve(server, 'localhost', 8081)
 
 print("started")
 asyncio.get_event_loop().run_until_complete(start_server)
