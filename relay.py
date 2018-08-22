ecb8da6b114caf433dcf33e5dca052c92067548c
Martin Vondráček <vondracek.mar@gmail.com>
2018-08-22 18:08:06 -0400

    relay using multiprocessing

--- relay.py
+++ relay.py
@@ -2,6 +2,8 @@
 
 import asyncio
 import functools
+from multiprocessing import Process, Pipe
+import os
 
 import websockets
 import msgpack
@@ -36,7 +38,7 @@ async def forward_up(down_socket, up_socket, queue_client_2_mitm):
         log(down_socket, up_socket, msg)
         queue_client_2_mitm.put(msg_raw)
         await up_socket.send(msg_raw)
-    asyncio.sleep(0.04)
+    await asyncio.sleep(0.04)
 
 
 async def forward_down(down_socket, up_socket, queue_mitm_2_client):
@@ -44,16 +46,17 @@ async def forward_down(down_socket, up_socket, queue_mitm_2_client):
         msg = msgpack.unpackb(msg_raw)
         log(up_socket, down_socket, msg)
         await down_socket.send(msg_raw)
-    asyncio.sleep(0.04)
+    await asyncio.sleep(0.04)
 
 
 async def mitm2client(down_socket, queue_mitm_2_client):
     logging.info('mitm2client queue_mitm_2_client.qsize()==' + str(queue_mitm_2_client.qsize()))
+    logging.info('queue_mitm_2_client.GET')
     try:
         msg = queue_mitm_2_client.get(timeout=0.050)
     except queue.Empty:
         pass
-        # logging.debug('queue_client_2_mitm empty')
+        logging.debug('queue_client_2_mitm empty')
     else:
         logging.info('>>>>>>>>> MITM -> client queue_mitm_2_client msg:' + str(msg))
         await down_socket.send(msg)
@@ -68,7 +71,7 @@ async def relay(down_socket, path, queue_client_2_mitm, queue_mitm_2_client):
             await asyncio.ensure_future(asyncio.gather(
                 forward_up(down_socket, up_socket, queue_client_2_mitm),
                 forward_down(down_socket, up_socket, queue_mitm_2_client),
-                mitm2client(down_socket, queue_mitm_2_client)
+                mitm2client(down_socket, queue_mitm_2_client),
             ))
 
 
@@ -101,30 +104,86 @@ async def ear_register(socket, path, queue_client_2_mitm, queue_mitm_2_client):
         except asyncio.TimeoutError:
             pass
         else:
-            logging.info('///// MITM -> CLIENT queue_mitm_2_client msg:' + str(msg))
+            logging.info('ear_register ///// MITM -> CLIENT queue_mitm_2_client msg:' + str(msg))
             queue_mitm_2_client.put(msg)
-            logging.info('queue_mitm_2_client.qsize()==' + str(queue_mitm_2_client.qsize()))
+            logging.info('ear_register queue_mitm_2_client.qsize()==' + str(queue_mitm_2_client.qsize()))
 
 
-queue_client_2_mitm = queue.Queue()
-queue_mitm_2_client = queue.Queue()
+# queue_client_2_mitm = queue.Queue()
+# queue_mitm_2_client = queue.Queue()
 
-# Logging
-logging.basicConfig(level=logging.DEBUG)
-logger_websockets = logging.getLogger('websockets')
-logger_websockets.setLevel(logging.INFO)
-logger_websockets.addHandler(logging.StreamHandler())
-logging.getLogger().setLevel(logging.DEBUG)
 
-start_ear = websockets.serve(functools.partial(
-    ear_register,
-    queue_client_2_mitm=queue_client_2_mitm,
-    queue_mitm_2_client=queue_mitm_2_client), myIP, 8090)
-start_relay = websockets.serve(functools.partial(
-    relay,
-    queue_client_2_mitm=queue_client_2_mitm,
-    queue_mitm_2_client=queue_mitm_2_client), myIP, 8080)
+# start_ear = websockets.serve(functools.partial(
+#     ear_register,
+#    queue_client_2_mitm=queue_client_2_mitm,
+#    queue_mitm_2_client=queue_mitm_2_client), myIP, 8090)
+# start_relay = websockets.serve(functools.partial(
+#    relay,
+#    queue_client_2_mitm=queue_client_2_mitm,
+#    queue_mitm_2_client=queue_mitm_2_client), myIP, 8080)
 
-logging.info("started")
-asyncio.get_event_loop().run_until_complete(asyncio.gather(start_relay, start_ear))
-asyncio.get_event_loop().run_forever()
+# logging.info("started")
+# asyncio.get_event_loop().run_until_complete(asyncio.gather(start_relay, start_ear))
+# asyncio.get_event_loop().run_forever()
+
+
+# ################################################################
+def client_receiver(client2mitm_pipe, mitm2client_pipe):
+    async def serve_connection(websocket, path, client2mitm_pipe, mitm2client_pipe):
+        logging.info(f"CONNECTED client {websocket.remote_address}")
+        cs = Process(target=client_sender, args=(websocket, mitm2client_pipe))
+        cs.start()
+        try:
+            while True:
+                logging.debug('client_receiver: waiting on websocket.recv')
+                msg_raw = await websocket.recv()
+                msg = msgpack.unpackb(msg_raw)
+                logging.info(f"FROM {websocket.remote_address} unpacked:{msg}")
+                client2mitm_pipe.send((websocket.remote_address, msg_raw))
+        finally:
+            logging.info(f"DISCONNECTED client {websocket.remote_address}")
+            cs.join() # TODO maybe await
+
+    logging_config()
+    logging.debug(
+        f'client_receiver: module name: {__name__}, parent process: {os.getppid()}, process id:{os.getpid()}.')
+    logging.debug('client_receiver: starting server')
+    asyncio.get_event_loop().run_until_complete(websockets.serve(functools.partial(
+        serve_connection, client2mitm_pipe=client2mitm_pipe, mitm2client_pipe=mitm2client_pipe), 'localhost', 8080))
+    asyncio.get_event_loop().run_forever()
+    logging.debug('client_receiver: server terminated')
+
+
+def client_sender(websocket, mitm2client_pipe):
+    logging_config()
+    logging.info(f'client_sender: module name: {__name__}, parent process: {os.getppid()}, process id:{os.getpid()}.')
+    while True:
+        logging.debug('client_sender: waiting on mitm2client_pipe')
+        remote_address, msg_raw = mitm2client_pipe.recv()
+        logging.debug('client_sender: sending')
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(websocket.send(msg_raw))
+        loop.close()
+        logging.debug('client_sender: sent ok')
+
+
+def logging_config():
+    logging.basicConfig(level=logging.DEBUG)
+    logger_websockets = logging.getLogger('websockets')
+    logger_websockets.setLevel(logging.INFO)
+    logger_websockets.addHandler(logging.StreamHandler())
+    logging.getLogger().setLevel(logging.DEBUG)
+
+
+def main():
+    logging_config()
+    logging.info(f'main module name: {__name__}, parent process: {os.getppid()}, process id:{os.getpid()}. ')
+
+    client2mitm_pipe, mitm2client_pipe = Pipe()
+    cr = Process(target=client_receiver, args=(client2mitm_pipe, mitm2client_pipe))
+    cr.start()
+    cr.join()
+
+
+if __name__ == '__main__':
+    main()
