7082c7c0364b5518959449578699eaad60ed905b
Martin Vondráček <vondracek.mar@gmail.com>
2018-09-04 14:12:11 -0400

    sig-server cleanup

--- sig-server.py
+++ sig-server.py
@@ -2,31 +2,15 @@
 
 import asyncio
 import functools
-
 import websockets
 import msgpack
 import logging
-import queue
 
 SIGNAL1 = "signal.bigscreenvr.com"
 SIGNAL2 = "signal2.bigscreenvr.com"
 SIGNAL3 = "signal3.bigscreenvr.com"
 signalServerURL = SIGNAL2
 myIP = '172.26.104.1'
-
-
-upstreamUrl = 'wss://'+signalServerURL
-upstreamUrl = 'ws://'+"localhost:8081" # server.py
-ear_socket = None
-
-
-# region UI inject
-#    initDebug(); UNITYVERSION='0.34.0';NAME="foo<script>signalServerURL=SIGNAL1=SIGNAL2=SIGNAL3='172.26.104.1:8090'; _0x6af9[959]='ws://';connectToSignalServer();</script>bar"
-# endregion
-
-# msg[b'type'] in [b'offer', b'answer', b'ice']:
-# logging.info(f"FROM {socket_from.remote_address[0]} TO {socket_to.remote_address[0]} unpacked:{msg}")
-
 CLIENTS = set()
 
 
@@ -37,15 +21,11 @@ async def forward(websocket, path):
         msg_raw = await websocket.recv()
         msg = msgpack.unpackb(msg_raw)
         for other_socket in CLIENTS.difference([websocket]):
-            logging.debug(f"FROM {websocket.remote_address} TO {other_socket.remote_address} unpacked:{msg}")
+            logging.info(f"FROM {websocket.remote_address} TO {other_socket.remote_address} unpacked:{msg}")
             await other_socket.send(msg_raw)
 
 # Logging
-logging.basicConfig(level=logging.DEBUG)
-logger_websockets = logging.getLogger('websockets')
-logger_websockets.setLevel(logging.INFO)
-logger_websockets.addHandler(logging.StreamHandler())
-logging.getLogger().setLevel(logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 
 start_relay = websockets.serve(functools.partial(forward), myIP, 8090)
 
